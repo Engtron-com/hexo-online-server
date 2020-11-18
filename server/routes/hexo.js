24dc50764d10c9979ee5e274f40cb78f6bb1555c
lizy <1593735671@qq.com>
2020-11-18 17:37:25 +0800

    文章界面优化

--- server/routes/hexo.js
+++ server/routes/hexo.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res, next) {
         if (req.query.pjax) {
             res.render('post');
         } else {
-            res.render('index', { wsPort: olConfig.wsPort, path: "hexo", ssl: olConfig.ssl });
+            res.render('index', { wsPort: olConfig.wsPort, path: "post", ssl: olConfig.ssl });
         }
     } else {
         res.render('login', { script: '' });
