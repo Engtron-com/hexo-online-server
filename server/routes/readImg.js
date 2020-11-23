42d50e6ab46c576110034ed7a708516acc860321
lizy <1593735671@qq.com>
2020-11-23 17:27:59 +0800

    图片获取问题

--- server/routes/readImg.js
+++ server/routes/readImg.js
@@ -4,10 +4,10 @@ var fs = require("fs");
 var path = require("path");
 
 /* GET img. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
     let imgPath = req.query.path.substring(1, req.query.path.length);
     let dirName = imgPath.replace("%23", "");
-    fs.readFile(path.join(hexo.source_dir, dirName),'binary', function (err, data) {
+    await fs.readFile(path.join(hexo.source_dir, dirName),'binary', function (err, data) {
         if (err) {
             send("读取图片"+dirName+"失败", "error");
             res.json({ success: false, data: err });
