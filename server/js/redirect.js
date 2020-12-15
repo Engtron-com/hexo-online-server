85157fe92afd91d06e0c9c5b4a2b109bad5f1dec
lizy <1593735671@qq.com>
2020-12-15 14:55:43 +0800

    服务器断开跳转到登录界面问题

--- server/js/redirect.js
+++ server/js/redirect.js
@@ -1,22 +1,11 @@
 function  redirectHandle(xhr) {
-
-    var url = xhr.getResponseHeader("redirectUrl");
-    console.log("redirectUrl = " + url);
-
-    var enable = xhr.getResponseHeader("enableRedirect");
-
-    // if((enable == "true") && (url != "")){
-    //     var win = window;
-    //     while(win != win.top){
-    //         win = win.top;
-    //     }
-    //     win.location.href = url;
-    // }
+    if (xhr.status == 503) {
+        window.location.href = window.location.host + '/login';
+    }
 }
 
 $(function () {
     $(document).ajaxComplete(function (event, xhr, settings) {
-        console.log("ajaxComplete  adffdafadsaf")
         redirectHandle(xhr);
     })
 })
